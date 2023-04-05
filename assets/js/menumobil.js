c5cf30d232bb088201c9ecce086084ccc52640c6
zidjian <waldirmaidana@gmail.com>
2023-04-05 12:39:32 -0500

    Correcciones de click en los vinculos del menu mobil

--- assets/js/menumobil.js
+++ assets/js/menumobil.js
@@ -2,6 +2,7 @@
     const icono_menu = document.querySelector('.icono_menu');
     const mennu_mobil = document.querySelector('.navegacionmobil');
     const icono_cerrar = document.querySelector('.icono_cerrar');
+    const mobil_link = document.querySelectorAll('.navegacionmobil-link');
 
     const toggleMenu = () => {
         mennu_mobil.classList.toggle('activo');
@@ -9,4 +10,7 @@
 
     icono_menu.addEventListener('click', toggleMenu);
     icono_cerrar.addEventListener('click', toggleMenu);
+    mobil_link.forEach((link) => {
+        link.addEventListener('click', toggleMenu);
+    });
 })();
